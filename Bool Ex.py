6d0016e75766e447833327548f41da2620d4549a
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:47:01 +0700

    Add files via upload

---
+++ Bool Ex.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(type(True))
+print(type(False))
+
+
+# In[3]:
+
+
+print(True)
+print(False)
+
+
+# In[4]:
+
+
+print(True and False)
+
+
+# In[5]:
+
+
+print(True or False)
+
+
+# In[ ]:
+
+
+
+
