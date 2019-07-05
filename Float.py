c203bc1cc3885c9312ff1128c1c615f145a998a2
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:45:23 +0700

    Add files via upload
    Float

---
+++ Float.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print(10+3.14)
+
+
+# In[3]:
+
+
+print(10+3.10)
+
+
+# In[4]:
+
+
+print(10+ 0.3)
+
+
+# In[5]:
+
+
+print(15*1.0)
+
+
+# In[6]:
+
+
+print(15.1*2)
+
+
+# In[7]:
+
+
+print(1j*1j)
+
+
+# In[ ]:
+
+
+
+
