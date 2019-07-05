d369a7c6ca9ef5360fc7ebac95be1a70aa62de38
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:50:15 +0700

    Add files via upload

---
+++ datatype .py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+print("Wellcome to PloyShop ")
+print(".....................................  ")
+print("Water   1   10   THB         ")
+print("Lay       2   40    THB        ")
+print("Testo    1   10   THB         ")
+
+
+# In[10]:
+
+
+print("Wellcome to PloyShop ")
+print("..........................................")
+print("Water", 1 , 10 , "THB",1*10, "THB",       )
+print("Lay"   ,  2 , 40 , "THB" ,2*20,"THB",     )
+print("Testo",  1 , 10, "THB" ,1*10, "THB",     )
+print("Total", 10+40+10  ,         "THB"     )
+
+
+# In[ ]:
+
+
+
+
