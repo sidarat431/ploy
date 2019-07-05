a84061065569673199ef3276757769c4ae6da0b3
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:42:52 +0700

    Add files via upload
    Information

---
+++ Ployy.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print(type(10))
+
+
+# In[3]:
+
+
+print(type("Hello"))
+
+
+# In[4]:
+
+
+print(type(True))
+
+
+# In[6]:
+
+
+print(type(10.3))
+
+
+# In[7]:
+
+
+print(type(1j))
+
+
+# In[8]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
