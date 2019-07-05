583016ab0e8807418c9ee07d64afd6da2393ac6d
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:47:43 +0700

    Add files via upload

---
+++ Str Ex.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello")
+print(type("Hello"))
+print("Hello+world")
+
+
+# In[3]:
+
+
+print("Hello"-"world")
+
+
+# In[4]:
+
+
+print("Hello"*5)
+
+
+# In[6]:
+
+
+print("Hello"/5)
+
+
+# In[ ]:
+
+
+
+
