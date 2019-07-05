c547fad377a6f33789e1488044741965df8d618e
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-05 16:55:31 +0700

    Add files via upload

---
+++ รายการสินค้า.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("PLoy Shop ")
+print("-----------------------------------------")
+print("1.water x 1 10 THB = 10 THB")
+print("2.Lay x 3 10  THB = 30 THB")
+print("3.Icecream x 3 10 THB = 30 THB")
+print("------------------------------------------")
+print("Total                          =70 THB  ")
+
+
+# In[ ]:
+
+
+
+
