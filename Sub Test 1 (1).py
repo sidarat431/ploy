976ca0142781192d279682b19890488d38dfc282
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-07-12 14:25:02 +0700

    Add files via upload

---
+++ Sub Test 1 (1).py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+print("#นางสาว สิดารัสมิ์   ทั่งทอง 362515241015 EE36241N")
+print("#นางสาวสุดารัตน์    อ่อนดี  362515241017 EE36241N")
+print("RT= 100 Ohm.")
+print("R2= 300 Ohm.")
+print("RT=(R1 * R2) / (R1+R2)")
+print("RT= 75 Ohm.")
+
+
+# In[ ]:
+
+
+
+
