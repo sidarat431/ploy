f7bce4c2b06e71f2d7b5eb113340fb5e07cd01ed
sidarat431 <52315140+sidarat431@users.noreply.github.com>
2019-09-06 21:44:36 +0700

    Add files via upload

---
+++ Temp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#นางสาวสิดารัศมิ์  ทั่งทอง 362515241015  EE36241N
+C = float(input("Fill out C : ") )
+F = ((9/5) * C) + 32
+K = C +  273.15
+print(F, "F")
+print(K, "K")
+
